5b32436d66ca7e912205f22d680c2fcb9e5c95f6
mmingoo <aldrn0323@naver.com>
2025-06-17 18:51:53 -0700

    star5 6 7 입니다(8은 작성 중)

---
+++ src/star8.java
@@ -0,0 +1,14 @@
+public class star8 {
+    public static void main(String[] args) {
+        for(int i  = 0; i < 5; i++){
+            for(int j = 0; j <2*i; j++ ){
+                if (4-i <= j && 4+i >= j){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
