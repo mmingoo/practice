14d1631d26bd95aff68b66099b61e48abf7c126a
mmingoo <aldrn0323@naver.com>
2025-08-04 09:44:20 +0900

    쓰레드 yield 예제

--- src/star/star9.java
+++ src/star/star9.java
@@ -1,7 +1,17 @@
 package star;
 
-public class star9 {
+class Star9 {
     public static void main(String[] args) {
-
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+//				if(!((i+j<=6 && i<=j )||(i>=j && i+j>=6))){  // 나비 모양
+                if((i+j<=6 && i<=j )||(i>=j && i+j>=6)){
+                    System.out.printf("[%d,%d]",i,j);
+                } else {
+                    System.out.print("     ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
